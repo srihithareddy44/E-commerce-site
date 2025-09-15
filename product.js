6b0cfcc7381ae55d37c66908e55560c2afe12372
srihithareddy44 <srihithareddy44@gmail.com>
2025-09-15 22:38:41 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,5 @@
+const products = [
+  { id: 1, name: "Red Shirt", price: 25, img: "https://i.pinimg.com/originals/7c/c4/19/7cc419279b99ed3180fa8fc393b02f11.png" },
+  { id: 2, name: "Blue Jeans", price: 40, img: "http://cdn.shopify.com/s/files/1/0223/4813/products/classic-blue-jeans-blue-denim-detail.jpg?v=1461339663" },
+  { id: 3, name: "Black Shoes", price: 60, img: "https://c8.alamy.com/comp/HR93FB/blue-jeans-black-shoes-HR93FB.jpg" }
+];
